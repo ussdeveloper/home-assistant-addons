a53c2eb98a34af45a0d147f5945a191fae048c6f
plusina <przemyslaw.lusina@psignite.com>
2025-10-21 14:36:52 +0200

    Add logging to run.sh and redirect output

--- run.sh
+++ run.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 
+echo "Starting Tauron Reader addon..."
+
 # Read config
 if [ -f /data/options.json ]; then
     SCHEDULE=$(jq -r '.schedule.times[]' /data/options.json | tr '\n' ' ')
+    echo "Schedule times: $SCHEDULE"
 else
     SCHEDULE="02:00 10:00"
+    echo "Using default schedule: $SCHEDULE"
 fi
 
 # Create cron jobs
 echo "# Tauron Reader cron jobs" > /etc/crontabs/root
 for time in $SCHEDULE; do
-    echo "$time * * * /app/tauron-reader" >> /etc/crontabs/root
+    echo "$time * * * /app/tauron-reader >> /proc/1/fd/1 2>&1" >> /etc/crontabs/root
+    echo "Added cron job: $time"
 done
 
+echo "Cron jobs created. Starting cron daemon..."
+
 # Start cron daemon
-crond -f
+crond -f -l 8
